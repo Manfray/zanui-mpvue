546d8737a9a3b7b61c85263a646b0fbc47a1becc
Mistergong <960940021@qq.com>
2018-09-08 20:22:23 +0800

    zanui for Mpvue 原始版备份

---
+++ src/pages/zan_dashboard/config.js
@@ -0,0 +1,84 @@
+export default {
+
+  base: {
+    title: '基础组件',
+    content: [{
+      name: 'Badge 徽章',
+      path: '/pages/zan_badge/main'
+    }, {
+      name: 'Button 按钮',
+      path: '/pages/zan_btn/main'
+    }, {
+      name: 'Capsule 胶囊',
+      path: '/pages/zan_capsule/main'
+    }, {
+      name: 'Card 卡片',
+      path: '/pages/zan_card/main'
+    }, {
+      name: 'Cell 单元格',
+      path: '/pages/zan_cell/main'
+    }, {
+      name: 'Helper 基础样式',
+      path: '/pages/zan_helper/main'
+    }, {
+      name: 'Icon 图标',
+      path: '/pages/zan_icon/main'
+    }, {
+      name: 'Layout 布局',
+      path: '/pages/zan_layout/main'
+    }, {
+      name: 'Loadmore 加载',
+      path: '/pages/zan_loadmore/main'
+    }, {
+      name: 'Noticebar 通告栏',
+      path: '/pages/zan_noticebar/main'
+    }, {
+      name: 'Panel 面板',
+      path: '/pages/zan_panel/main'
+    }, {
+      name: 'Popup 弹出层',
+      path: '/pages/zan_popup/main'
+    }, {
+      name: 'Select 选择',
+      path: '/pages/zan_select/main'
+    }, {
+      name: 'Stepper 计数器',
+      path: '/pages/zan_stepper/main'
+    }, {
+      name: 'Steps 步骤条',
+      path: '/pages/zan_steps/main'
+    }, {
+      name: 'Switch 开关',
+      path: '/pages/zan_switch/main'
+    }, {
+      name: 'Tab 标签',
+      path: '/pages/zan_tab/main'
+    }, {
+      name: 'Tag 标记',
+      path: '/pages/zan_tag/main'
+    }]
+  },
+  form: {
+    title: '表单',
+    content: [{
+      name: 'Field 输入框',
+      path: '/pages/zan_field/main'
+    }]
+  },
+  action: {
+    title: '操作反馈',
+    content: [{
+      name: 'Actionsheet 行动按钮',
+      path: '/pages/zan_actionsheet/main'
+    }, {
+      name: 'Dialog 弹出框',
+      path: '/pages/zan_dialog/main'
+    }, {
+      name: 'Toast 轻提示',
+      path: '/pages/zan_toast/main'
+    }, {
+      name: 'TopTips 顶部提示',
+      path: '/pages/zan_toptips/main'
+    }]
+  }
+}
